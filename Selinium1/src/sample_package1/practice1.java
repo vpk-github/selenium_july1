4b4c71f727e8547856c3c4815db1a5b8e827b8af
LENOVO <LENOVO@DESKTOP-FMTB188>
2025-07-01 13:12:00 +0530

    secon commit

---
+++ Selinium1/src/sample_package1/practice1.java
@@ -0,0 +1,14 @@
+package sample_package1;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.annotations.Test;
+
+public class practice1 {
+
+	@Test
+	public void demo()
+	{
+		WebDriver driver = new ChromeDriver();
+	}
+}
